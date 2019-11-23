220b94c63dcf236458d6daa4eb76a8f1324bd1d1
René Zarwel <darenegade@gmx.de>
2019-11-23 12:33:09 +0100

    comp. mode

--- frontend/sit-o-mat/rollup.config.js
+++ frontend/sit-o-mat/rollup.config.js
@@ -1,7 +1,6 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+//import { createDefaultConfig } from '@open-wc/building-rollup';
+//export default createDefaultConfig({ input: './index.html' });
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
-
-export default createDefaultConfig({ input: './index.html' });
+import { createCompatibilityConfig } from '@open-wc/building-rollup';
+export default createCompatibilityConfig({ input: './index.html' });
